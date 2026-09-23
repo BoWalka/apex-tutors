1ffb44a56788c052ba08350d0f9001dd57764eff
bowalka <59brimm@gmail.com>
2026-09-22 23:17:53 -0400

    deployment changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // appDir is now stable and enabled by default in Next.js 13+
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
